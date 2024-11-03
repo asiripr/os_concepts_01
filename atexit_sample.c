f2a183b4321cdd12e72b2261fd47ea90717710fc
asiripr <asiripramodaya2000@gmail.com>
2024-11-03 08:42:48 +0530

    os codes

--- atexit_sample.c
+++ atexit_sample.c
@@ -1,1 +1,13 @@
-:wq
+#include <stdio.h>
+#include <stdlib.h>
+
+void exitfunc(){
+    printf("called cleanup function - exitfunc()\n");
+    return;
+}
+
+int main(){
+    atexit(exitfunc);
+    printf("Hello Sri Lanka\n");
+    exit (0);
+}
---
+++ parentprocess_nowit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ int main(){
+    int pid;
+    pid = fork();
+
+    // child process
+    if (pid == 0)
+    {
+        system("ps -ef");
+        sleep(10); 
+        system("ps -ef"); 
+    }else
+    {
+        sleep(3);
+    }
+    return 0;
+    
+ }
